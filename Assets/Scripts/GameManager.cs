d25b28354e4106f96b1fd43230b4c2d65e65bd86
Christian <christian.wrona@gmail.com>
2021-02-14 18:51:18 -0500

    Added Board and BasePiece

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Board mBoard;
+    
+    public PieceManager mPieceManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Create the board
+        mBoard.Create();
+
+        // Create pieces
+        mPieceManager.Setup(mBoard);
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
---
+++ Assets/Scripts/Pieces/BasePiece.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public abstract class BasePiece : EventTrigger
+{
+    [HideInInspector]
+    public Color mColor = Color.clear;
+    public bool mIsFirstMove = true;
+
+    protected Cell mOriginalCell = null;
+    protected Cell mCurrentCell = null;
+
+    protected RectTransform mRectTransform = null;
+    protected PieceManager mPieceManager;
+
+    protected Cell mTargetCell = null;
+
+    protected Vector3Int mMovement = Vector3Int.one;
+    protected List<Cell> mHighlightedCells = new List<Cell>();
+
+    public virtual void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager)
+    {
+        mPieceManager = newPieceManager;
+
+        mColor = newTeamColor;
+        GetComponent<Image>().color = newSpriteColor;
+        mRectTransform = GetComponent<RectTransform>();
+    }
+
+    public virtual void Place(Cell newCell)
+    {
+        //Cell Stuff
+        mCurrentCell = newCell;
+        mOriginalCell = newCell;
+        mCurrentCell.mCurrentPiece = this;
+
+        //Object Stuff
+        transform.position = newCell.transform.position;
+        gameObject.SetActive(true);
+    }
+
+
+    // public void Reset()
+    // {
+
+    // }
+
+    // public virtual void Kill()
+    // {
+
+    // }
+
+    #region Movement
+    private void CreateCellPath(int xDirection, int yDirection, int movement)
+    {
+        // Target position
+        int currentX = mCurrentCell.mBoardPosition.x;
+        int currentY = mCurrentCell.mBoardPosition.y;
+
+        // Check each cell
+        for(int i = 1; i <= movement; i++)
+        {
+            currentX += xDirection;
+            currentY += yDirection;
+
+            // TODO: Get the state of the target cell
+
+            // Add to list
+            mHighlightedCells.Add(mCurrentCell.mBoard.mAllCells[currentX, currentY]);
+        }
+    }
+
+    protected virtual void CheckPathing()
+    {
+        // Horizontal
+        CreateCellPath(1, 0, mMovement.x);
+        CreateCellPath(-1, 0, mMovement.x);
+
+        // Vertical
+        CreateCellPath(0, 1, mMovement.y);
+        CreateCellPath(0, -1, mMovement.y);
+
+        // Upper diagonal
+        CreateCellPath(1, 1, mMovement.z);
+        CreateCellPath(-1, 1, mMovement.z);
+
+        // Lower diagonal
+        CreateCellPath(-1, -1, mMovement.z);
+        CreateCellPath(1, -1, mMovement.z);
+    }
+
+
+
+    protected void ShowCells()
+    {
+        foreach (Cell cell in mHighlightedCells)
+            cell.mOutlineImage.enabled = true;
+    }
+
+    protected void ClearCells()
+    {
+        foreach (Cell cell in mHighlightedCells)
+            cell.mOutlineImage.enabled = false;
+        
+        mHighlightedCells.Clear();
+    }
+    #endregion
+
+    // protected virtual void Move()
+    // {
+
+    // }
+    // #endregion
+
+    #region Events
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        // Test for cells
+        CheckPathing();
+
+        // Show valid cells
+        ShowCells();
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        // Follow pointer
+        transform.position += (Vector3)eventData.delta;
+
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        // Hide
+        ClearCells();
+    }
+    #endregion
+}
